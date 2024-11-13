1a9c2a3f8bbcc9ac6c56f68f48e22817208a8ac1
ivan <ilapk@mail.ru>
2024-11-14 03:17:18 +0400

    init

---
+++ XFlowTestTask/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Cleanup
+{
+    internal class Program
+    {
+        private const double TargetChangeTime = 1;
+
+        private double _previousTargetSetTime;
+        private bool _isTargetSet;
+        
+        private dynamic _lockedCandidateTarget;
+        private dynamic _lockedTarget;
+        private dynamic _target;
+        private dynamic _previousTarget;
+        private dynamic _activeTarget;
+        private dynamic _targetInRangeContainer;
+
+        public void CleanupTest(Frame frame)
+        {
+            try
+            {
+                TryResetLockedTarget(_lockedCandidateTarget);
+                TryResetLockedTarget(_lockedTarget);
+
+                _isTargetSet = false;
+
+                // Sets _activeTarget field
+                TrySetActiveTargetFromQuantum(frame);
+
+                _isTargetSet = CanKeepTarget();
+                _previousTarget = _target;
+
+                if(_isTargetSet) return;
+                
+                if (TrySetTarget(_lockedTarget)) return;
+                if (TrySetTarget(_activeTarget)) return;
+                
+                TrySetTargetFromContainer(_targetInRangeContainer);
+            }
+            finally
+            {
+                ApplyTarget();
+            }
+        }
+
+        private void TryResetLockedTarget(dynamic lockedTarget)
+        {
+            if (lockedTarget && !lockedTarget.CanBeTarget)
+            {
+                lockedTarget = null;
+            }
+        }
+
+        /// <summary>
+        /// If target exists and can be targeted, it should stay within Target Change Time since last target change
+        /// </summary>
+        /// <returns></returns>
+        private bool CanKeepTarget() => _target && _target.CanBeTarget && IsWithinTimeSinceLastTargetChange();
+        private bool IsWithinTimeSinceLastTargetChange() => Time.time - _previousTargetSetTime < TargetChangeTime;
+
+        private bool TrySetTarget(dynamic fromTarget)
+        {
+            if (!fromTarget || !fromTarget.CanBeTarget) 
+                return false;
+            
+            _target = fromTarget;
+            _isTargetSet = true;
+            return true;
+
+        }
+
+        private void TrySetTargetFromContainer(dynamic targetInRangeContainer)
+        {
+            _target = targetInRangeContainer.GetTarget();
+
+            if (_target)
+            {
+                _isTargetSet = true;
+            }
+        }
+
+        private void ApplyTarget()
+        {
+            if (_isTargetSet)
+            {
+                SavePreviousTargetTime();
+            }
+            else
+            {
+                _target = null;
+            }
+
+            TargetableEntity.Selected = _target;
+        }
+
+        private void SavePreviousTargetTime()
+        {
+            if (_previousTarget != _target)
+            {
+                _previousTargetSetTime = Time.time;
+            }
+        }
+    }
+}
